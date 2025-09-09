97444f90ae9d81724b38b15e9d407b396a9a1bcd
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-09-09 22:30:37 +0530

    q1

---
+++ src/Day1/q3.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class q3 {
+    public static void main (String[]args){
+        int t1=12,t2=10;
+        if(t1>t2){
+            System.out.println("Varun");
+        }else if(t1==t2){
+            System.out.println("Tie");
+        }else{
+            System.out.println("Arjun");
+        }
+    }
+}
