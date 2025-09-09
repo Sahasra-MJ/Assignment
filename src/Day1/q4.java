30df3ff5ae0a5311fe8f8f0d25574f2dcfc48ba6
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-09-09 22:36:28 +0530

    q1

--- src/Day1/q4.java
+++ src/Day1/q4.java
@@ -1,13 +1,13 @@
 package Day1;
 
 public class q4 {
-    public static void main(String[]args){
-        int a=50,b=100,c=75;
-        if(a>=b && a>=c){
+    public static void main(String[]args) {
+        int a = 50, b = 100, c = 75;
+        if (a >= b && a >= c) {
             System.out.println(a);
-        }else if(b>=a && b>=c){
+        } else if (b >= a && b >= c) {
             System.out.println(b);
-        }else{
+        } else {
             System.out.println(c);
         }
     }
