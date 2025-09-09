4a8d195377c7d963746d6a9618357641bf9b2a5d
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-09-09 22:17:14 +0530

    Initial commit

---
+++ src/q11.java
@@ -0,0 +1,10 @@
+public class q11 {
+    public static void main(String[] args) {
+        int n=4;
+        if(n%2==0){
+            System.out.println("ON");
+        }else{
+            System.out.println("OFF");
+        }
+    }
+}
