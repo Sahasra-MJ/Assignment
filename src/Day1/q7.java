0370df8e46f6eb1b97f236db464da4ec9a637be9
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-09-10 18:07:14 +0530

    q1

---
+++ src/Day1/q7.java
@@ -0,0 +1,22 @@
+package Day1;
+
+public class q7 {
+    public static void main(String[]args){
+        int a=5,b=2;
+        char op=3;
+        switch(op){
+            case 1:
+                System.out.println(a+b);
+                break;
+            case 2:
+                System.out.println(a-b);
+                break;
+            case 3:
+                System.out.println(a*b);
+                break;
+            case 4:
+                System.out.println(a/b);
+                break;
+        }
+    }
+}
