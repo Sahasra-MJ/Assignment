b8b5e0ad5cf40052600158fd78679bed30fb1bc6
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-09-16 18:25:15 +0530

    second largest element

--- src/Day2/SecondLargest.java
+++ src/Day2/SecondLargest.java
@@ -2,7 +2,7 @@ package Day2;
 
 import java.util.Arrays;
 
-public class secondLargest {
+public class SecondLargest {
     public static void main(String[]args){
         int arr[] = {14, 28, 19, 33, 45, 12};
         Arrays.sort(arr);
