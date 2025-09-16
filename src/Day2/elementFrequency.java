2eb115cf48b610521e9162aa0b1308bcb9f5cb87
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-09-16 18:15:18 +0530

    Frequency of element

---
+++ src/Day2/elementFrequency.java
@@ -0,0 +1,18 @@
+package Day2;
+public class elementFrequency {
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 2, 6, 2, 9, 6};
+        boolean visited[] = new boolean[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i]) continue;
+            int freq = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    freq++;
+                    visited[j] = true;
+                }
+            }
+            System.out.printf("%d appears %d times\n", arr[i], freq);
+        }
+    }
+}
