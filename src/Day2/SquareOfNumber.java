1914ee429cf6acac6ffe44c4524573a8b8d2315e
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-09-16 18:31:47 +0530

    Square of a number

---
+++ src/Day2/SquareOfNumber.java
@@ -0,0 +1,9 @@
+package Day2;
+
+public class SquareOfNumber {
+    public static void main(String[] args) {
+        for(int i=1;i<=15;i++){
+            System.out.printf("%d*%d=%d\n",i,i,i*i);
+        }
+    }
+}
