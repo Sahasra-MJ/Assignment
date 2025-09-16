281c1dfd1740c45beeae64f02a60aba5e1f3717b
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-09-16 18:55:08 +0530

    Pyramid of stars

---
+++ src/Day2/StarPyramid.java
@@ -0,0 +1,15 @@
+package Day2;
+public class StarPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.printf(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.printf("*");
+            }
+            System.out.printf("%n");
+        }
+    }
+}
