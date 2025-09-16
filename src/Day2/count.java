a353b842f4811d5a1f79b5ede30b38ef70435a4d
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-09-16 17:59:23 +0530

    Count pos,neg,zero

---
+++ src/Day2/count.java
@@ -0,0 +1,20 @@
+package Day2;
+
+public class count {
+    public static void main(String[]args){
+        int arr[] = {5, -3, 0, 12, -9, 0, 7};
+        int positive=0,negative=0,zero=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0){
+                positive++;
+            }else if(arr[i]<0) {
+                negative++;
+            }else{
+                zero++;
+            }
+        }
+        System.out.println(positive);
+        System.out.println(negative);
+        System.out.println(zero);
+    }
+}
