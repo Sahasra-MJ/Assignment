4358b11a5e7031eca6253160697e34241abb5c37
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-09-16 18:04:10 +0530

    Second Largest

---
+++ src/Day2/secondLargest.java
@@ -0,0 +1,11 @@
+package Day2;
+
+import java.util.Arrays;
+
+public class secondLargest {
+    public static void main(String[]args){
+        int arr[] = {14, 28, 19, 33, 45, 12};
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-2]);
+    }
+}
