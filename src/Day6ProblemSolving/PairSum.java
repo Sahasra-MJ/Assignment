fe25d2af1d813c74bfc96d50c5e37c5c88798ee2
Sahasra Oliyarasi <sahasramayildevv@gmail.com>
2025-10-15 18:21:33 +0530

    week6PS

---
+++ src/Day6ProblemSolving/PairSum.java
@@ -0,0 +1,15 @@
+package Day6ProblemSolving;
+
+public class PairSum {
+    public static void main(String[] args) {
+        int arr[]={1,5,3,0,6,2};
+        int k=6;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==k){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+}
